777e79950c8e33c1024622b54fe8efab1bea347c
Steve Rothier <steve.rothier@etudiant.univ-reims.fr>
2022-03-20 20:20:45 +0100

    Contact zone text

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -4,52 +4,58 @@ function Contact() {
   return (
     <>
         <div className='ContactBox' id='#contact'>
-            <div>
-                <form className='formContact'>
-                    <div>
-                        <input 
-                            className='inputSize'
-                            type='text' 
-                            name='nom' 
-                            id='nom'
-                            placeholder='Nom'
-                            required
-                        >
-                        </input>
-                    </div>
-                    <div>
-                        <input 
-                            className='inputSize'
-                            type='email' 
-                            name='email' 
-                            id='email'
-                            placeholder='Adresse e-mail'
-                            required
-                        >
-                        </input>
-                    </div>
-                    <div>
-                        <textarea 
-                            className='textareaSize'
-                            name="message"
-                            id="message" 
-                            placeholder='Votre message'
-                        >
-                        </textarea>
-                    </div>
-                    <div>
-                        <input 
-                            className='submitInputSize'
-                            type='submit' 
-                            name='submit' 
-                            id='submit'
-                            value='Me contacter'
-                            required
-                        >
-                        </input>
-                    </div>
-                </form>
+            <div className='contactZone'>
+                <div className='contactStyle'>
+                    Contact
+                </div>
+                <div className='questionStyle'>
+                    Une question ? Contacter moi.
+                </div>
             </div>
+            <form className='formContact'>
+                <div>
+                    <input 
+                        className='inputSize'
+                        type='text' 
+                        name='nom' 
+                        id='nom'
+                        placeholder='Nom'
+                        required
+                    >
+                    </input>
+                </div>
+                <div>
+                    <input 
+                        className='inputSize'
+                        type='email' 
+                        name='email' 
+                        id='email'
+                        placeholder='Adresse e-mail'
+                        required
+                    >
+                    </input>
+                </div>
+                <div>
+                    <textarea 
+                        className='textareaSize'
+                        name="message"
+                        id="message" 
+                        placeholder='Votre message'
+                    >
+                    </textarea>
+                </div>
+                <div>
+                    <input 
+                        className='submitInputSize'
+                        type='submit' 
+                        name='submit' 
+                        id='submit'
+                        value='Me contacter'
+                        required
+                    >
+                    </input>
+                </div>
+            </form>
         </div>
     </>
   );
