b201d50841f0041571c5c3669fa8b29f179b6a96
Steve Rothier <steve.rothier@etudiant.univ-reims.fr>
2022-03-21 23:24:44 +0100

    Profil component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import Contact from './component/Contact/Contact';
 import Navbar from './component/Navbar/Navbar';
 import Footer from './component/Footer/Footer';
+import Profil from './component/Profil/Profil';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Profil />
       <Contact />
       <Footer />
     </>
---
+++ src/component/Profil/Profil.js
@@ -0,0 +1,47 @@
+import './Profil.css';
+import myHead from '../../assets/myHead.png';
+import cv from '../../assets/CV.pdf#toolbar=0';
+import { AiOutlineDownload } from "react-icons/ai";
+
+function Profil() {
+  return (
+    <>
+        <div className='ProfilBox'>
+            <div className='myProfil'>
+                <div className='myStatus'>
+                    <div className='myName'>Steve ROTHIER</div>
+                    <img className='myHead' src={myHead} alt=''></img>
+                    <div className='status'>Etudiant en DUT informatique</div>
+                </div>
+                <div>
+                    <div className='statusBlock'>                        
+                        Passionné par l’informatique depuis 
+                        que l’ai découverts, je suis curieux d'acquérir de nouvelles 
+                        connaissances. Dans ce domaine plusieurs thèmes m'intéresse 
+                        comme le développement Web, la cybersécurité, 
+                        le montage photo comme vidéo ou encore le design.
+                    </div>
+                    <div className='myCv'>
+                        <p>Vous souhaitez avoir mon CV au format PDF c'est par </p>
+                        <a href={cv}>ici.</a> | 
+                        <a href={cv} download> <AiOutlineDownload className='myCvDownload'/></a>
+                    </div>
+                </div>
+            </div>
+            <div className='myCvNone'>
+                <p>Vous souhaitez avoir mon CV au format PDF c'est par </p>
+                <a href={cv}>ici.</a> |
+                <div>
+                    <a href={cv} download> <AiOutlineDownload className='myCvDownload'/></a>
+                </div>
+            </div>
+            <div className='myCvNonePhone'>
+                <div>Vous souhaitez avoir mon CV au format PDF </div>
+                <a href={cv} download> <AiOutlineDownload className='myCvDownload'/></a>
+            </div>
+        </div>
+    </>
+  );
+}
+
+export default Profil;
