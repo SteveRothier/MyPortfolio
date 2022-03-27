b84ad212e8f15f084ac9b4fa35ccb93595eccd86
Steve Rothier <steve.rothier@etudiant.univ-reims.fr>
2022-03-27 08:12:58 +0200

    style reworked of news

--- src/component/New/NewsList.js
+++ src/component/New/NewsList.js
@@ -1,14 +1,6 @@
 import './News.css';
 import intership from '../../assets/intership.jpg';
 import iut from '../../assets/iut.png';
-import dut from '../../assets/dut.jpg';
-import NewsItem from './NewsItem';
-
-const stageDescription =    "Je suis à la recherche d'un stage en informatique pour valider ma derrière année de DUT informatique à l'IUT de Reims. " +
-                            " Ce stage de 10 à 12 semaines à pour but de m'initier dans ma vie professionnel avec un projet à la réalisation.";
-
-const dutDescription =  "Ui " +
-                        ""
 
 function NewsList() {
   return (
@@ -23,22 +15,48 @@ function NewsList() {
                 </div>
             </div>
             <div className='newsItemsList'>
-                <NewsItem 
-                    titre='Recherche de stage'
-                    image={intership}
-                    date='21/03/2022'
-                    actif='True'
-                    description={stageDescription}
-                    imageModal={intership}
-                />
-                <NewsItem 
-                    titre='DUT informatique'
-                    image={iut}
-                    date='01/09/2020'
-                    actif='True'
-                    description={dutDescription}
-                    imageModal={dut}
-                />
+
+                <div className='NewsItemBox'>
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <div className='newsTitle'>Recherche de stage</div>
+                            <img src={intership} alt=''></img>
+                            <div className='date'>21/03/2022</div>
+                            <div className='bar'></div>
+                            <div className='actifClass'>Actif= 
+                                <p className='actif'>True</p>
+                            </div>
+                        </div>
+                        <div class="flip-card-back">
+                            <p>
+                                Je suis à la recherche d'un stage en informatique 
+                                pour valider ma derrière année de DUT informatique 
+                                à l'IUT de Reims. Ce stage de 10 à 12 semaines à pour but 
+                                de m'initier dans ma vie professionnel avec un projet à 
+                                la réalisation.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='NewsItemBox'>
+                    <div class="flip-card-inner">
+                        <div class="flip-card-front">
+                            <div className='newsTitle'>DUT informatique</div>
+                            <img src={iut} alt=''></img>
+                            <div className='date'>01/09/2020</div>
+                            <div className='bar'></div>
+                            <div className='actifClass'>Actif= 
+                                <p className='actif'>True</p>
+                            </div>
+                        </div>
+                        <div class="flip-card-back">
+                            <p>
+
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </>
