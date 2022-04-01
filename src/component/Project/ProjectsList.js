288ae96b34db49501d272a1645f23b20066fe5ae
Steve Rothier <steve.rothier@etudiant.univ-reims.fr>
2022-04-01 10:21:17 +0200

    adding some project descriptions

--- src/component/Project/ProjectsList.js
+++ src/component/Project/ProjectsList.js
@@ -10,6 +10,19 @@ import metier from '../../assets/metier.png';
 import tandem from '../../assets/tandem.png';
 import visuel from '../../assets/visuel.png';
 
+import { FaGithub } from "react-icons/fa";
+import { BsFileEarmarkPdfFill } from "react-icons/bs";
+import { FaFileDownload } from "react-icons/fa";
+
+
+import terminal from '../../assets/terminal.pdf#toolbar=0';
+import TPDroit from '../../assets/TPDroit.pdf#toolbar=0';
+import TPGestion from '../../assets/TPGestion.pdf#toolbar=0';
+import vet_consut from '../../assets/vet_consut.pdf#toolbar=0';
+import Dossier_documentaire from '../../assets/Dossier_documentaire.pdf#toolbar=0';
+import Teams from '../../assets/Teams.pdf#toolbar=0';
+import ProjetS4 from '../../assets/ProjetS4.pdf#toolbar=0';
+
 
 function ProjectsList() {
   return (
@@ -38,6 +51,11 @@ function ProjectsList() {
                         <div className="flip-card-back">
                             <p>Ce portfolio me permet de montrer mon profil en tant que developpeur</p> 
                             <p>Créer entierement en ReactJS</p>
+                            <p>
+                                <a href='https://github.com/SteveRothier/myPortfolio' className='firstLink'> 
+                                    <FaGithub className='ReseauLogo github'/>
+                                </a>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -54,7 +72,18 @@ function ProjectsList() {
                             <div className='date'>17/03/2022</div>
                             </div>
                         <div className="flip-card-back">
-
+                            <p>
+                                <div>
+                                    <a href={ProjetS4} className='firstLink'>
+                                        <BsFileEarmarkPdfFill className='pdf ReseauLogo'/>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href={ProjetS4} className='lastLink' download>
+                                      <FaFileDownload className='download ReseauLogo'/>
+                                    </a>                                
+                                </div>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -71,7 +100,29 @@ function ProjectsList() {
                             <div className='date'>08/03/2022</div>
                             </div>
                         <div className="flip-card-back">
-
+                            <p>
+                                full impro blabla
+                            </p>
+                            <p>
+                                <div>
+                                    Voir le pdf 
+                                    <a href={Dossier_documentaire} className='firstLink'>
+                                        <BsFileEarmarkPdfFill className='pdf ReseauLogo'/>
+                                    </a>
+                                </div>
+                                <div>
+                                  Télécharger le pdf
+                                  <a href={Dossier_documentaire} className='lastLink' download>ici.</a>                                
+                                </div>
+                                <div>
+                                    Voir le pdf 
+                                    <a href={Teams}>ici.</a>
+                                </div>
+                                <div>
+                                  Télécharger le pdf
+                                  <a href={Teams} className='lastLink' download>ici.</a>                                
+                                </div>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -88,7 +139,18 @@ function ProjectsList() {
                             <div className='date'>05/01/2022</div>
                             </div>
                         <div className="flip-card-back">
-
+                            <p>
+                                <div>
+                                    <a href={TPGestion} className='firstLink'>
+                                        <BsFileEarmarkPdfFill className='pdf ReseauLogo'/>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href={TPGestion} className='lastLink' download>
+                                        <FaFileDownload className='download ReseauLogo'/>
+                                    </a>                                
+                                </div>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -105,8 +167,24 @@ function ProjectsList() {
                             <div className='date'>17/12/2021</div>
                             </div>
                         <div className="flip-card-back">
-                            <p>Le but de ce projet est de réaliser un site Web pour un vétérinaire
-afin d’améliorer l’optimisation de son emploi du temps.</p>
+                            <p>
+                                Le but de ce projet est de réaliser
+                                un site Web pour un vétérinaire
+                                afin d’améliorer l’optimisation 
+                                de son emploi du temps.
+                            </p>
+                            <p>
+                                <div>
+                                    <a href={vet_consut} className='firstLink'>
+                                        <BsFileEarmarkPdfFill className='pdf ReseauLogo'/>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href={vet_consut} className='lastLink' download>
+                                        <FaFileDownload className='download ReseauLogo'/>   
+                                    </a>                                
+                                </div>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -123,7 +201,18 @@ afin d’améliorer l’optimisation de son emploi du temps.</p>
                             <div className='date'>14/12/2021</div>
                             </div>
                         <div className="flip-card-back">
-
+                        <p>
+                                <div>
+                                    <a href={TPDroit} className='firstLink'>
+                                        <BsFileEarmarkPdfFill className='pdf ReseauLogo'/>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href={TPDroit} className='lastLink' download>
+                                        <FaFileDownload className='download ReseauLogo'/>
+                                    </a>                                
+                                </div>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -170,7 +259,11 @@ afin d’améliorer l’optimisation de son emploi du temps.</p>
                                 interview d'une personne 
                                 travaillant dans le métier 
                                 choisi.
-
+                            </p>
+                            <p>
+                                <a href='https://github.com/SteveRothier/ProjetS1' className='firstLink'> 
+                                    <FaGithub className='ReseauLogo github'/>
+                                </a>
                             </p>
                         </div>
                     </div>
@@ -197,6 +290,23 @@ afin d’améliorer l’optimisation de son emploi du temps.</p>
                                 tout dans une application mobile android
                                 simple d'utilisation.
                             </p>
+                            <p>
+                                Réaliser sous MIT App Inventor afin de faire
+                                une application android et d'un site web permettant
+                                d'interagir avec l'emploi du temps de l'appication.
+                            </p>
+                            <p>
+                                <div>
+                                    <a href={terminal} className='firstLink'>
+                                        <BsFileEarmarkPdfFill className='pdf ReseauLogo'/>
+                                    </a>
+                                </div>
+                                <div>
+                                    <a href={terminal} className='lastLink' download>
+                                        <FaFileDownload className='download ReseauLogo'/>
+                                    </a>                                
+                                </div>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -214,7 +324,19 @@ afin d’améliorer l’optimisation de son emploi du temps.</p>
                             </div>
                         <div className="flip-card-back">
                             <p>
-                                
+                                Un projet d'échange scolaire entre le lycée Stephen Hessel d'Epernay
+                                et les élèves de la Realschule Anne Frank 
+                                et du Gymnasium Albertus d’Ettlingen.
+                            </p>
+                            <p>
+                                Le but était, pour chaque classe, de construire une 
+                                éolienne d’1m30 en utilisant des matériaux fabriqués, 
+                                récupérés, transformés ou détournés.
+                            </p>
+                            <p>
+                                Notre groupe de STI2D avait pour but de réaliser le système 
+                                capable de recharger des appareils électroniques 
+                                sous Arduino.
                             </p>
                         </div>
                     </div>
